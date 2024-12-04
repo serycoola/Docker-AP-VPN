c34f87165b403e77fba9171581ceee97f2236801
serycoola <71021622+serycoola@users.noreply.github.com>
2024-12-05 00:52:25 +0200

    Update vpnconnect.sh

--- vpnconnect.sh
+++ vpnconnect.sh
@@ -1,1 +1,19 @@
+#!/bin/bash -e
+
+# Default path for OpenVPN configs
+true ${VPN_PATH:="/ovpn"}
+# Set path for OpenVPN configs.
+vpn_path=${VPN_PATH}
+
+# Change directory to working path (in order to avoid OpenVPN looking for credentials in /)
+cd "$path"
+
+# Default config to load
+true ${CONFIG:=".ovpn"}
+# Partial or full name of the config you want to load, just enough info to be able to identify a single file in the folder when using *
+config=${CONFIG} 
+
+# Connect to VPN
+openvpn --config *"$config"*
+
 
