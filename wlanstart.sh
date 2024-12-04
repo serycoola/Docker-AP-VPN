8fb30c557cc0bb434a1805026cad1be21cce1278
serycoola <71021622+serycoola@users.noreply.github.com>
2024-12-05 01:46:05 +0200

    Update wlanstart.sh

--- wlanstart.sh
+++ wlanstart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+### BEGIN HOTSPOT CONFIGURATION ###
+
 # Default values
 true ${INTERFACE:=wlan0}
 true ${OUTGOINGS:=tun0}
@@ -118,4 +120,30 @@ dhcpd ${INTERFACE}
 
 echo "Starting HostAP daemon ..."
 /usr/sbin/hostapd /etc/hostapd.conf 
+
+### END HOTSPOT CONFIGURATION ###
+
+
+
+### BEGIN VPN CONFIGURATION ###
+
+# Default path for OpenVPN configs
+true ${VPN_PATH:="/ovpn"}
+# Set path for OpenVPN configs.
+vpn_path=${VPN_PATH}
+
+# Change directory to working path (in order to avoid OpenVPN looking for credentials in /)
+cd "$vpn_path"
+
+# Default config to load
+true ${CONFIG:=".ovpn"}
+# Partial or full name of the config you want to load, just enough info to be able to identify a single file in the folder when using *
+config=${CONFIG} 
+
+# Connect to VPN
+openvpn --auth-nocache --config *"$config"*
+
+### END VPN CONFIGURATION ###
+
+
 
